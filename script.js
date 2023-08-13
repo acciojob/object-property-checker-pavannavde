79f0b5902ed671e5e5ebbe8f60ca348387c2c691
Pavan Navde <136722639+pavannavde@users.noreply.github.com>
2023-08-13 11:54:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,6 +2,11 @@ const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
 
 function hasKey(key) {
   //   write your code here
+	if(sampleObject[key]==undefine)
+	{
+		return false;
+	}
+	return true;
 }
 
 // Do not change the code below
